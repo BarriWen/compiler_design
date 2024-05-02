a0ad4f3fef3e0911d033e825562e1f718f83a904
BarriWen <wenzelongkyrie@outlook.com>
2024-05-02 14:46:42 -0400

    add build2.sh

--- build2.sh
+++ build2.sh
@@ -5,5 +5,5 @@ javac CodeGen_Visitor.java
 echo "compiled java files"
 cd tests; gcc -c -S print.c
 cd ..
-java PA5 < tests/tests/demo_array.c > tests/tests/demo_array.s
+java PA5 < tests/demo_array.c > tests/demo_array.s
 cd tests; gcc demo_array.s print.s -o demo_array; ./demo_array
