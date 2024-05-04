15335c883cf41e0d76e01455a8aa546ecca8b4eb
BarriWen <wenzelongkyrie@outlook.com>
2024-05-04 13:57:30 -0400

    update build3

--- build3.sh
+++ build3.sh
@@ -5,7 +5,7 @@ javac CodeGen_Visitor.java
 javac JavaToCConverter.java
 cd tests; gcc -c -S print.c
 cd ..
-java JavaToCConverter tests/mjPP.java output_file.c
+java JavaToCConverter tests/mjPP.java tests/output_file.c
 java PA5 < tests/output_file.c > tests/output_file.s
 cd tests; gcc output_file.s print.s -o output_file; ./output_file
 
