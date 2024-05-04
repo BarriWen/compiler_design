d3f8b24f682ca0753d3904ad69d8585c06c7f19a
BarriWen <wenzelongkyrie@outlook.com>
2024-05-04 16:57:56 -0400

    update pa6

--- build3.sh
+++ build3.sh
@@ -1,11 +1,8 @@
-#!/usr/bin/bash
+!/usr/bin/bash
 java -classpath javacc.jar javacc PA5.jj
 javac PA5.java
-javac CodeGen_Visitor.java
-javac JavaToCConverter.java
+javac Arm_Visitor.java
 cd tests; gcc -c -S print.c
 cd ..
-java JavaToCConverter tests/mjPP.java tests/output_file.c
-java PA5 < tests/output_file.c > tests/output_file.s
-cd tests; gcc output_file.s print.s -o output_file; ./output_file
-
+java PA5 < tests/demo.c > tests/demo1.s
+cd tests; gcc demo1.s print.s -o demo1; ./demo1
